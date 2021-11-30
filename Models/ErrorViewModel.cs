2c894e294b12721df34a3d2bc14fa28bb80b732e
pascalvdbosch <pascalvdbosch@hotmail.com>
2021-11-30 12:34:30 +0100

    mijnwebapp

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace _2021_11_30_12_33_29.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
